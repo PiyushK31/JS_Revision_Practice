9881b07fe865c6a074d1fee8961ffa8be11882a8
Piyush Kumar <piyushkrr80580@gmail.com>
2026-04-28 11:05:14 +0000

    datatypes

---
+++ 01_Basic/Test.js
@@ -0,0 +1,12 @@
+var name = "Piyush"; //srting
+let age = 21;  //numbers
+const contry = "india" // string
+let x ; //undifine
+console.log(x);
+const p = null;  //null
+console.log(p);
+let isThis = true;  //BOLLEAN
+console.log(isThis);
+let y = Object;  //function object
+console.log(y);
+
